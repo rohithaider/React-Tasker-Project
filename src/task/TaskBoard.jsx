283e52ee13dbb4b15722bc8f1bb81df98bbed8ca
rohithaider <rohithaider@gmail.com>
2024-10-16 20:27:05 +0600

    task list modal created

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 export default function TaskBoard() {
   //creating this default data object so that i can initialize it to the tasks initial state instead
@@ -17,16 +18,20 @@ export default function TaskBoard() {
   };
 
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal,setShowAddModal] = useState(false);
+
+ 
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal/>}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
+          <TaskActions onAddClick={()=> setShowAddModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
