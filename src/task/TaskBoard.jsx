ea96288d3a86fafb92ea6f9170ae5f9e032eec8f
rohithaider <rohithaider@gmail.com>
2024-10-16 19:39:44 +0600

    Dynamically added the tasks from the data objects passed through from the parent props

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  //creating this default data object so that i can initialize it to the tasks initial state instead
+  //of undefined.
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to learn react so that I can be a successful front-end developer",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask/>
+          <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions/>
-          <TaskList/>
+          <TaskActions />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
