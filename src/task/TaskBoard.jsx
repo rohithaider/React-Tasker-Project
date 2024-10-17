1686e9d01413fd53fc9fb29e78180666f620402c
rohithaider <rohithaider@gmail.com>
2024-10-17 12:18:29 +0600

    add task feature implemented

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -20,11 +20,17 @@ export default function TaskBoard() {
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal,setShowAddModal] = useState(false);
 
+  function handleAddTask(newTask){
+    
+    setTasks([...tasks, newTask])
+    setShowAddModal(false);
+
+  }
  
 
   return (
     <section className="mb-20" id="tasks">
-      {showAddModal && <AddTaskModal/>}
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
